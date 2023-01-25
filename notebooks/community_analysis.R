9b1088630ef78d4ac0c3ca635ec64c742e3c597d
karkman <antti.karkman@gmail.com>
2023-01-25 19:21:27 +0200

    draft of all notebooks

---
+++ notebooks/community_analysis.R
@@ -0,0 +1,17 @@
+# Bacterial and fungal community analysis
+
+lorem ipsum
+
+First we need to load some libraries.  
+
+```{r}
+library(tidyverse)
+library(phyloseq)
+library(vegan)
+library(microViz)
+```
+
+```{r}
+bac_raw <- readRDS()
+fun_raw <- readRDS()
+```
---
+++ notebooks/dada2.R
@@ -0,0 +1,8 @@
+# DADA2 pipeline
+
+Libraries
+
+```{r}
+library(dada2)
+library(phyloseq)
+```
